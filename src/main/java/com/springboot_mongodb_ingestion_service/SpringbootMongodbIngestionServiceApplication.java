1d03a053eb7cc3db5e88f266e84e8ad3da9cd5ae
shivam-java <dishivam.1652@gmail.com>
2023-01-22 20:37:04 +0530

    initial commit

---
+++ src/main/java/com/springboot_mongodb_ingestion_service/SpringbootMongodbIngestionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.springboot_mongodb_ingestion_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMongodbIngestionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMongodbIngestionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot_mongodb_ingestion_service/SpringbootMongodbIngestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot_mongodb_ingestion_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMongodbIngestionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
